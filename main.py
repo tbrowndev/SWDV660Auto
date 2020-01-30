fab81bd9be3991f921175550299ee1167fa35edc
tbrowndev <46692709+tbrowndev@users.noreply.github.com>
2020-01-29 21:30:33 -0600

    init
    created method that takes a word and return military spelling of that work

---
+++ main.py
@@ -0,0 +1,13 @@
+def MilitarySpelling(word):
+    militaryAlphabetDictionary = {'a':"Alpha",'b':"Bravo",'c':"Charlie",'d':"Delta",'e':"Echo",'f':"Foxtrot",'g':"Golf",'h':"Hotel",'i':"India",'j':"Juliet",'k':"Kilo",'l':"Lima",'m':"Mike",'n':"November",'o':"Oscar",'p':"Papa",'q':"Quebec",'r':"Romeo",'s':"Sierra",'t':"Tango",'u':"Uniform",'v':"Vistor",'w':"Whiskey",'x':"X-Ray",'y':"Yankee",'z':"Zulu"}
+    wordSpellingMilitary = ""
+    for char in word:
+        wordSpellingMilitary = wordSpellingMilitary + militaryAlphabetDictionary.get(char.lower(), char.lower()) + " - "
+    return wordSpellingMilitary[0:len(wordSpellingMilitary)-3]
+
+def Main():
+    word = input("Enter a Word (No SPACES): ")
+    print("Military Spellig of your word:")
+    print(MilitarySpelling(word))
+
+Main()
